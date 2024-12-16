bedf8efa7211945d42443036bdb03dac052d88a4
bulma1 <lyruz2354@gmail.com>
2024-12-16 16:06:02 +0700

    Update response text

--- ch5/sample-app/app.test.js
+++ ch5/sample-app/app.test.js
@@ -5,10 +5,6 @@ describe('Test the app', () => {
   test('Get / should return Hello, World!', async () => {
     const response = await request(app).get('/'); 
     expect(response.statusCode).toBe(200);        
-<<<<<<< HEAD
-    expect(response.text).toBe('Fundamentals of DevOps!');  
-=======
     expect(response.text).toBe('Fundamentals of DevOps!!');  
->>>>>>> 35714c7 (update res text in test)
   });
 });
